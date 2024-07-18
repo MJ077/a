974cddba26d8948e7f4b6b1ddc3240e92d126af0
Muhammadjon <muhammadjonkomilov88@icloud.com>
2024-07-18 15:58:33 +0500

    Add files via upload

---
+++ 1-masala.py
@@ -0,0 +1,8 @@
+def ajratib_olish(sonlar):
+    musbat = [a for a in sonlar if a > 0]
+    return musbat
+
+
+list1 = [1, -1, 2, 9, -3, -11, 20, 5, -8, 4]
+list2 = ajratib_olish(list1)
+print(list2) 
